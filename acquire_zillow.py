7ba090ddf1110d89373d951f9f142b9385eb2476
Paige Guajardo <paigeeguajardo@gmail.com>
2021-12-16 16:07:20 -0600

    Updated files

--- acquire_zillow.py
+++ acquire_zillow.py
@@ -30,20 +30,49 @@ def get_zillow_data():
     if os.path.isfile(filename):
         zillow = pd.read_csv(filename)
     else:
-        zillow = pd.read_sql("""SELECT *
-FROM properties_2017
-INNER JOIN (SELECT parcelid, max(transactiondate) AS max_date, logerror
-FROM predictions_2017
-GROUP BY parcelid, logerror) AS sub1 using(parcelid)
-LEFT JOIN propertylandusetype using(propertylandusetypeid)
-LEFT JOIN airconditioningtype using(airconditioningtypeid)
-LEFT JOIN architecturalstyletype using(architecturalstyletypeid)
-LEFT JOIN buildingclasstype using(buildingclasstypeid)
-LEFT JOIN heatingorsystemtype using(heatingorsystemtypeid)
-LEFT JOIN typeconstructiontype USING (typeconstructiontypeid)
-LEFT JOIN storytype using(storytypeid)
-WHERE properties_2017.latitude IS NOT NULL AND properties_2017.longitude IS NOT NULL AND max_date LIKE '2017%%'
-AND propertylandusetypeid = 261 OR 279;
+        zillow = pd.read_sql("""
+#SELECT *
+#FROM properties_2017
+#INNER JOIN (SELECT parcelid, max(transactiondate) AS max_date, logerror
+#FROM predictions_2017
+#GROUP BY parcelid, logerror) AS sub1 using(parcelid)
+#LEFT JOIN propertylandusetype using(propertylandusetypeid)
+#LEFT JOIN airconditioningtype using(airconditioningtypeid)
+#LEFT JOIN architecturalstyletype using(architecturalstyletypeid)
+#LEFT JOIN buildingclasstype using(buildingclasstypeid)
+#LEFT JOIN heatingorsystemtype using(heatingorsystemtypeid)
+#LEFT JOIN typeconstructiontype USING (typeconstructiontypeid)
+#LEFT JOIN storytype using(storytypeid)
+#WHERE properties_2017.latitude IS NOT NULL AND properties_2017.longitude IS NOT NULL AND max_date LIKE '2017%%'
+#AND propertylandusetypeid = 261 OR 279;
+# Ravinders Code For code up
+
+SELECT prop.*, 
+       pred.logerror, 
+       pred.transactiondate, 
+       air.airconditioningdesc, 
+       arch.architecturalstyledesc, 
+       build.buildingclassdesc, 
+       heat.heatingorsystemdesc, 
+       landuse.propertylandusedesc, 
+       story.storydesc, 
+       construct.typeconstructiondesc 
+FROM   properties_2017 prop  
+       INNER JOIN (SELECT parcelid,
+       					  logerror,
+                          Max(transactiondate) transactiondate 
+                   FROM   predictions_2017 
+                   GROUP  BY parcelid, logerror) pred
+               USING (parcelid) 
+       LEFT JOIN airconditioningtype air USING (airconditioningtypeid) 
+       LEFT JOIN architecturalstyletype arch USING (architecturalstyletypeid) 
+       LEFT JOIN buildingclasstype build USING (buildingclasstypeid) 
+       LEFT JOIN heatingorsystemtype heat USING (heatingorsystemtypeid) 
+       LEFT JOIN propertylandusetype landuse USING (propertylandusetypeid) 
+       LEFT JOIN storytype story USING (storytypeid) 
+       LEFT JOIN typeconstructiontype construct USING (typeconstructiontypeid) 
+WHERE  prop.latitude IS NOT NULL 
+       AND prop.longitude IS NOT NULL AND transactiondate <= '2017-12-31';
 """, 
         get_connection('zillow'))
         zillow.to_csv(index = False)
@@ -194,7 +223,69 @@ def yearbuilt_years(df):
 #--------------------------------------------------------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------------------------------------------------------
+# Function to read and wrangle data:
+def handle_missing_values(df, prop_required_row = .5, prop_required_col = .7):
+    ''' function which takes in a dataframe, required notnull proportions of non-null rows and columns.
+    drop the columns and rows columns based on theshold:'''
+    
+    #drop columns with nulls
+    threshold = int(prop_required_col * len(df.index)) # Require that many non-NA values.
+    df.dropna(axis = 1, thresh = threshold, inplace = True)
+    
+    #drop rows with nulls
+    threshold = int(prop_required_row * len(df.columns)) # Require that many non-NA values.
+    df.dropna(axis = 0, thresh = threshold, inplace = True)
+    return df
 
+
+def remove_columns(df, cols_to_remove):  
+    df = df.drop(columns=cols_to_remove)
+    return df
+
+def wrangle_zillow_codeup(df):
+    #df = pd.read_csv('zillow.csv')
+    
+    # Already Specificy These two lines with setting typeid to 271 in SQL statement
+    # Restrict df to only properties that meet single unit use criteria
+    #single_use = [261, 262, 263, 264, 266, 268, 273, 276, 279]
+    #df = df[df.propertylandusetypeid.isin(single_use)]
+    
+    # Restrict df to only those properties with at least 1 bath & bed and 350 sqft area
+    df = df[(df.bedroomcnt > 0) & (df.bathroomcnt > 0) & ((df.unitcnt<=1)|df.unitcnt.isnull())\
+            & (df.calculatedfinishedsquarefeet>350)]
+
+    # Handle missing values i.e. drop columns and rows based on a threshold
+    df = handle_missing_values(df)
+    
+    # Add column for counties
+    df['county'] = np.where(df.fips == 6037, 'Los_Angeles',
+                           np.where(df.fips == 6059, 'Orange', 
+                                   'Ventura'))    
+    # drop columns not needed
+    list_of_dropped = ['id','calculatedbathnbr','finishedsquarefeet12','fullbathcnt','propertycountylandusecode','propertylandusetypeid','censustractandblock','propertylandusedesc']
+    # 'unitcnt' 'heatingorsystemdesc', 'propertyzoningdesc', 'heatingorsystemtypeid' 'buildingqualitytypeid'
+
+    df = remove_columns(df, list_of_dropped)
+
+
+    # replace nulls in unitcnt with 1
+    # df.unitcnt.fillna(1, inplace = True)
+    
+    # assume that since this is Southern CA, null means 'None' for heating system
+    # df.heatingorsystemdesc.fillna('None', inplace = True)
+    
+    # replace nulls with median values for select columns
+    df.lotsizesquarefeet.fillna(7313, inplace = True)
+    # df.buildingqualitytypeid.fillna(6.0, inplace = True)
+
+    # Columns to look for outliers
+    df = df[df.taxvaluedollarcnt < 5_000_000]
+    df[df.calculatedfinishedsquarefeet < 8000]
+    
+    # Just to be sure we caught all nulls, drop them here
+    df = df.dropna()
+    
+    return df
 #--------------------------------------------------------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------------------------------------------------------
